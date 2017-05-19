8899226fb40aa960636c5041fa571d6e2515084b
emesc <tahder77@gmail.com>
2017-05-19 12:48:49 +1000

    Added test for a completed project. Passed the test.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Project do
   describe "initialization" do
     let(:project) { Project.new }
     let(:task) { Task.new }
-    
+
     it "considers a project with no tasks to be done" do
       expect(project).to be_done
     end
@@ -13,6 +13,12 @@ RSpec.describe Project do
     it "knows that a project with an incomplete task is not done" do
       project.tasks << task
       expect(project).not_to be_done
+    end
+
+    it "marks a project done if its tasks are done" do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
     end
   end
 end
