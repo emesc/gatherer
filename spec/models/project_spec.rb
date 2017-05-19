52673a8ae37b5bb37bf1b16b13426bf5328993d2
emesc <tahder77@gmail.com>
2017-05-19 12:36:11 +1000

    Moved specs into the describe block.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Project do
   describe "initialization" do
     let(:project) { Project.new }
     let(:task) { Task.new }
-  end
+    
+    it "considers a project with no tasks to be done" do
+      expect(project).to be_done
+    end
 
-  it "considers a project with no tasks to be done" do
-    expect(project).to be_done
-  end
-
-  it "knows that a project with an incomplete task is not done" do
-    project.tasks << task
-    expect(project).not_to be_done
+    it "knows that a project with an incomplete task is not done" do
+      project.tasks << task
+      expect(project).not_to be_done
+    end
   end
 end
