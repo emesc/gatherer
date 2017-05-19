9f93c36a37d68d1f55a49cfd407188eb2f7a16db
emesc <tahder77@gmail.com>
2017-05-19 11:56:14 +1000

    Fixed matcher error. Made the test temporarily pass.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Project do
 
   it "considers a project with no tasks to be done" do
     project = Project.new
-    expect(project.done?).to be_truthy?
+    expect(project.done?).to be_truthy
   end
 end
