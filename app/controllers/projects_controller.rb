c09df440df73b2c28bd654eec129c994068e024f
emesc <em@curlycoder.me>
2017-06-08 16:17:42 +1000

    Just rewrote for a quick recap

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
 
+  def project_data
+    render json
+  end
+
   def new
     @project = Project.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resources :projects
+
+  get 'projects/project_data', defaults: { format: 'json' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
