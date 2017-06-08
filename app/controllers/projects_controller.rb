90da28838ea605f2c7d74073b464dbdd6eeed0d3
emesc <em@curlycoder.me>
2017-06-09 09:33:28 +1000

    Added create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,17 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @action = CreatesProject.new(
+      name: params[:project][:name],
+      task_string: params[:project][:tasks])
+    success = @action.create
+    if success
+      redirect_to projects_path
+    else
+      @project = @action.project
+      render :new
+    end
+  end
 end
