c142a5b7fc8037adfc135026f827ba8a2e968a8f
emesc <em@curlycoder.me>
2017-06-08 17:17:58 +1000

    Added test for handling a single string with size

--- spec/actions/creates_project_spec.rb
+++ spec/actions/creates_project_spec.rb
@@ -22,4 +22,12 @@ describe "task string parsing" do
     expect(tasks.map(&:title)).to eq(["Start things"])
     expect(tasks.map(&:size)).to eq([1])
   end
+
+  it "handles a single string with size" do
+    creator = CreatesProject.new(name: "Test", task_string: "Start things:3")
+    tasks = creator.convert_string_to_tasks
+    expect(tasks.size).to eq 1
+    expect(tasks.map(&:title)).to eq(["Start things"])
+    expect(tasks.map(&:size)).to eq([3])
+  end
 end
