68f49fcc6de0accd37a1a59ca5d7283e2e6fb299
emesc <em@curlycoder.me>
2017-06-14 12:34:42 +1000

    Defined sequence outside of factory and reused.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :task do
     sequence(:title) { |n| "Task #{n}" }
+
+    sequence :email do |n|
+      "user_#{n}@test.com"
+    end
+  end
+
+  factory :user do
+    name "Fred Flinstone"
+    email
   end
 end
