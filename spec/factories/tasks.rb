ba5434731674ea7a4df06140dfb344380c9c07f1
emesc <em@curlycoder.me>
2017-06-14 12:45:44 +1000

    Used traits which can hold multiple attributes

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,12 +2,32 @@ FactoryGirl.define do
   factory :task do
     sequence(:title) { |n| "Task #{n}" }
 
-    factory :big_task, parent: :task do
+    trait :large do
       size 5
     end
-  end
 
-  factory :small_task, parent: :task do
-    size 1
+    trait :small do
+      size 1
+    end
+
+    trait :soon do
+      due_date { 1.day.from_now }
+    end
+
+    trait :later do
+      due_date { 1.month.from_now }
+    end
+
+    # can be rewritten as factory :trivial, traits: [:small, :later]
+    factory :trivial do
+      small
+      later
+    end
+
+    # can be rewritten as factory :panic, traits: [:large, :soon]
+    factory :panic do
+      large
+      soon
+    end
   end
 end
