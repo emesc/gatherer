e64facd70297ef6a348493fca0475a96cbcb42be
emesc <em@curlycoder.me>
2017-06-14 11:48:40 +1000

    Modified factory to refer to an arbitrary class

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
-  factory :project do
-    name "Project Runway"
-    due_date Date.parse("2014-01-12")
+  factory :big_project, class: Project do
+    name "Big Project"
   end
 end
