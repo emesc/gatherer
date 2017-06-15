de669592b2b636e6ad199b5bf8ab8bb3617b50f8
emesc <em@curlycoder.me>
2017-06-15 12:51:12 +1000

    Added update action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,4 +24,13 @@ class ProjectsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice: "'Project was successfully updated.'"
+    else
+      render action: 'edit'
+    end
+  end
 end
